f0573f12c114588e5d8603bd41a7221a9eeb96ab
Peter Pfeufer <info@ppfeufer.de>
2022-01-04 20:16:46 +0100

    cleanup

--- celeryanalytics_housekeeping/__init__.py
+++
@@ -1,5 +0,0 @@
-default_app_config = (
-    "celeryanalytics_housekeeping.apps.CeleryanalyticsHousekeepingConfig"
-)
-
-__version__ = "0.0.1-alpha.2"
--- celeryanalytics_housekeeping/apps.py
+++
@@ -1,8 +0,0 @@
-# Django
-from django.apps import AppConfig
-
-
-class CeleryanalyticsHousekeepingConfig(AppConfig):
-    name = "celeryanalytics_housekeeping"
-    label = "celeryanalytics_housekeeping"
-    verbose_name = "Celery Analytics Housekeeping"
